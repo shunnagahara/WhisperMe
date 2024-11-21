54c95987241d58b6d73151ccab2730330b8e8af4
shunnagahara <ringorian0626@gmail.com>
2024-11-20 19:44:08 -0800

    開発環境＆本番環境用のfirebaseの切り分けを行なった

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,24 +2,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAjUBjzUARBI2GtQMj-2Jx0FWgm2BK98iA",
-  authDomain: "dev-lovyu.firebaseapp.com",
-  projectId: "dev-lovyu",
-  storageBucket: "dev-lovyu.firebasestorage.app",
-  messagingSenderId: "992940157334",
-  appId: "1:992940157334:web:3a04403a96690acebf55b7",
-  measurementId: "G-DL2EDD3SG7"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
